f6b6a72f9021b0e1bdc362a087d63d5c260939fd
sugawa-3 <sugawa.e.0112@gmail.com>
2020-11-10 21:07:27 +0900

    [fix]lint

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var test = "test";
+  let test = 'test';
   return (
-		<div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
